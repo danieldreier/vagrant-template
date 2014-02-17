dc412e88fc6493f2dcad04841f858bb98ace9843
Daniel Dreier <ddreier@thinkplango.com>
2014-02-16 23:46:23 -0800

    Fix puppet lint warnings and error

--- puppet/manifests/main.pp
+++ puppet/manifests/main.pp
@@ -1,14 +1,14 @@
 # script to run puppet
-file{"/usr/local/bin/runpuppet":
+file{'/usr/local/bin/runpuppet':
   content => " \
   sudo puppet apply -vv /vagrant/puppet/manifests/main.pp\n",
-  mode    => 0755
+  mode    => '0755',
 }
 
 # script to run librarian-puppet
-file{"/usr/local/bin/runlibrarian":
+file{'/usr/local/bin/runlibrarian':
   content => "cd /etc/puppet && sudo librarian-puppet update --verbose\n",
-  mode    => 0755
+  mode    => '0755',
 }
 
 import 'nodes.pp'
--- puppet/manifests/testcases/testcase-nfsmount.pp
+++ puppet/manifests/testcases/testcase-nfsmount.pp
@@ -1,12 +1,11 @@
 # Test case for PUP-1415
-# Requires the haraldsk/nfs module which librarian-puppet has problems installing
+# Requires the haraldsk/nfs module that librarian-puppet has problems installing
 # Tested on Centos 6.4
 # Basic tests of nfs export, mount, and operations on a root_squash'd NFS mount
 
 include nfs::server
 nfs::server::export{ '/export/repo':
   ensure  => 'mounted',
-  #clients => '10.0.0.0/24(rw,insecure,async,no_root_squash) localhost(rw)',
   clients => '10.0.0.0/24(rw,insecure,async,root_squash) localhost(rw)',
   require => File['/export/repo'],
 }
@@ -20,19 +19,19 @@ file { [ '/export', '/export/repo' ]:
 }
 
 file { '/export/repo/anybodycanwrite':
-  ensure => 'directory',
-  mode   => '0777',
+  ensure  => 'directory',
+  mode    => '0777',
   require => File['/export/repo'],
 }
 
 file { '/testrepo/anybodycanwrite/nfstest.sh':
-  ensure => 'present',
-  mode   => '0555',
+  ensure  => 'present',
+  mode    => '0555',
   content => "#!/bin/bash \n date > /tmp/currentdate",
   require => File['/export/repo/anybodycanwrite'],
 }
 
-exec { '/testrepo/anybodycanwrite/nfstest.sh': 
+exec { '/testrepo/anybodycanwrite/nfstest.sh':
   require => File['/testrepo/anybodycanwrite/nfstest.sh'],
 }
 
